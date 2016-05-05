00298d168b72252d03c3cda95f94de538c31730e
ManasaMona <manasaalisetty@gamil.com>
2016-05-05 19:32:20 +0530

    basic files created

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Login</title>
+</head>
+<body>
+<?php
+//login page
+
+?>
+ <form method='post' action=''>
+Email ID<input type="email" name="emailid" ><br>
+Password:<input type="password" name="passwrd" ><br>
+ <button type="submit"  value="Submit">Submit</button>
+       </form>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Register</title>
+</head>
+<body>
+<?php
+//Registeration page
+
+?>
+ <form method='post' action=''>
+ 
+UserName: <input type="text" name="username" ><br>
+Email ID<input type="email" name="emailid" ><br>
+Password:<input type="password" name="passwrd" ><br>
+Confirm Password<input type="password" name="cpword" ><br>
+Phone Number<input type="tel-num" name="cellnum" ><br>
+ <button type="submit"  value="Submit">Submit</button>
+       </form>
+</body>
+</html>
